654fd1f7377e87aa1c8b85148f47baba617dd3ad
Trista Chen <tristachentw@gmail.com>
2018-10-11 17:25:42 +0800

    enable network info in react-native debug tool

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from './App'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
+
+// enable network info in react-native debug tool
+global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest
